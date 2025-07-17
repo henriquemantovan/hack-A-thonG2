7a67e5d223fd729a7247340fdcb3abed461ff847
Henrique <henrique.mantovan@usp.br>
2025-07-17 03:17:49 -0300

    mudancinha para dar deploy

--- miniApp/src/pages/layout.tsx
+++ miniApp/src/pages/layout.tsx
@@ -4,10 +4,6 @@ import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: '[HACK-A-THON] MiniApp',
-  description: 'MiniApp do vendedor',
-};
 
 export default function RootLayout({
   children,
