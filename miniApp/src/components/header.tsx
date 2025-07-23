dec9edf4eccdd7384d93ac7fef20f682c826bdf4
kiyochi <danshoji@usp.br>
2025-07-23 17:30:13 -0300

    Teste dos hooks

--- miniApp/src/components/header.tsx
+++ miniApp/src/components/header.tsx
@@ -1,12 +1,36 @@
 'use client';
 
 import { TonConnectButton } from '@tonconnect/ui-react';
+import { useTonConnect } from '../hooks/useTonConnect';
+
 
 //ALGUEM CENTRALIZA PARA MIM PFV :(
 export default function Header() {
+    const { connected, wallet, network } = useTonConnect();
+  
   return (<header>
-    
+    <div>
+        {connected && (
+          <>
+            <div>
+              <p>Wallet: {wallet}</p>
+              <p>Network: {network}</p>
+              <p>Status: Connected</p>
+            </div>
+            <div/>
+          </>
+        )}
+        {!connected && (
+          <div>
+            <p>Status: Disconnected</p>
+          </div>
+        )}
+    </div>
+
+
       <TonConnectButton />
+    
     </header>
+
   );
 }
---
+++ miniApp/src/hooks/useTonConnect.tsx
@@ -0,0 +1,19 @@
+import { CHAIN, useTonConnectUI, useTonWallet } from "@tonconnect/ui-react";
+import { Address } from "ton-core";
+import { SenderArguments } from "ton-core";
+import { Sender } from "ton-core";
+
+export function useTonConnect(): {
+    connected: boolean;
+    wallet: string | null;
+    network: CHAIN | null;
+} {
+    const [tonConnectUI] = useTonConnectUI()
+    const wallet = useTonWallet()
+
+    return {
+        connected: !!wallet?.account.address,
+        wallet: wallet?.account.address ?? null,
+        network: wallet?.account.chain ?? null 
+    }
+}
