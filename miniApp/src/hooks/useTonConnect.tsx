b615813cdcbc8531b90472c0c43c8894f840bdba
kiyochi <danshoji@usp.br>
2025-07-24 12:32:30 -0300

    CONTRATO FUNCIONANDO

--- miniApp/src/hooks/useTonConnect.tsx
+++ miniApp/src/hooks/useTonConnect.tsx
@@ -1,19 +1,37 @@
 import { CHAIN, useTonConnectUI, useTonWallet } from "@tonconnect/ui-react";
-import { Address } from "@ton/core";
-import { SenderArguments } from "@ton/core";
-import { Sender } from "@ton/core";
+import { Address, Sender, SenderArguments } from "@ton/core";
 
 export function useTonConnect(): {
+    sender: Sender | null;
     connected: boolean;
     wallet: string | null;
     network: CHAIN | null;
 } {
-    const [tonConnectUI] = useTonConnectUI()
-    const wallet = useTonWallet()
-    
+    const [tonConnectUI] = useTonConnectUI();
+    const wallet = useTonWallet();
+
+    const sender: Sender | null = wallet?.account?.address
+        ? {
+            send: async (args: SenderArguments) => {
+                await tonConnectUI.sendTransaction({
+                    validUntil: Date.now() + 5 * 60 * 1000,
+                    messages: [
+                        {
+                            address: args.to.toString(),
+                            amount: args.value.toString(),
+                            payload: args.body?.toBoc().toString("base64") ?? undefined,
+                        },
+                    ],
+                });
+            },
+            address: Address.parse(wallet.account.address),
+        }
+        : null;
+
     return {
-        connected: !!wallet?.account.address,
-        wallet: wallet?.account.address ?? null,
-        network: wallet?.account.chain ?? null 
-    }
+        sender,
+        connected: !!wallet?.account?.address,
+        wallet: wallet?.account?.address ?? null,
+        network: wallet?.account?.chain ?? null,
+    };
 }
